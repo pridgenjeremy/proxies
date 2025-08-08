a31834773da39e258de6fb78f5a8ccf3a487fdcc
pridgenjeremy <pridgen.jeremy@icloud.com>
2025-08-08 12:46:07 +0000

    update

---
+++ ds.py
@@ -0,0 +1,25 @@
+from datasets import load_dataset
+from os import environ
+from mistralai import Mistral
+
+client = Mistral(environ.get("MISTRAL_API_TOKEN"))
+
+def salesforce_ds():
+  return load_dataset("Salesforce/xlam-function-calling-60k", token=environ.get("HF_API_TOKEN"), split="train")
+
+def translate(txt: str):
+  req = client.beta.conversations.start(
+    inputs=[
+      {"role": "user", "content": f"Traduzca al español latino:\n\n${txt}"}
+    ],
+    completion_args={
+      "temperature": 0.1
+    },
+    model="ft:ministral-3b-latest:29886b6b:20250727:0cdff515"
+  )
+  return req.model_dump()["outputs"][0]
+
+
+if __name__ == "__main__":
+  ds = salesforce_ds().shuffle()
+  print(translate(ds[0]["tools"]))
